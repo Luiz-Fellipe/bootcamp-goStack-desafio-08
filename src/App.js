799c77e8214d0aea678e56597bb7fcc5229f1cc3
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-03-01 14:30:35 -0300

    Adicionando estilos globais

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes />
     </BrowserRouter>
   );
