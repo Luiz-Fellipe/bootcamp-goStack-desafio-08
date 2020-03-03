59214a8c5624becde5f0313671a67bd475986281
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-03-03 19:04:28 -0300

    adicionando produtos ao carrinho

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
+import { connect } from 'react-redux';
 import { Container, Cart } from './styles';
 
 import logo from '../../assets/image/logo.svg';
 
-export default function Header() {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -14,10 +15,14 @@ export default function Header() {
       <Cart to="/cart">
         <div>
           <strong>Meu Carrinho</strong>
-          <span>3 intens</span>
+          <span>{cartSize} itens</span>
         </div>
         <MdShoppingBasket size={20} color="#fff" />
       </Cart>
     </Container>
   );
 }
+
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
